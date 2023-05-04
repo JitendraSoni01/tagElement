441ac7e840a6efc2c218c584a862e631a61e76e6
Jitendra SOni <jitendrasoni8226@gmail.com>
2023-05-04 16:47:10 +0530

    tag element

---
+++ tag.js
@@ -0,0 +1,13 @@
+// var items = documents.getElementByClass('li');
+// console.log(li);
+// li[2].style.backgroundColor = 'yellow';
+// li[1].style.fontWeight = 'bold';
+// li[1].textContent = 'Hellow';
+
+//gives error
+
+items.styles.backgroundColor = '#f4f4f4';
+
+for(var i=0;i<li.length;i++) {
+    li[i].style.backgroundColor = '#f4f4f4'
+}
